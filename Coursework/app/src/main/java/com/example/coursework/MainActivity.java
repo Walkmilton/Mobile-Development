488cace2db65df8ba18077f183a14c41a5dcb43b
Declan <dex.doyle@me.com>
2019-03-24 16:29:45 +0000

    Created basic navigation

--- Coursework/app/src/main/java/com/example/coursework/MainActivity.java
+++ Coursework/app/src/main/java/com/example/coursework/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.coursework;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void viewFridge(View view) {
+        Intent intent = new Intent(this, FridgeActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewRecipies(View view){
+        Intent intent = new Intent(this, RecipeActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewList(View view) {
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
+    }
+
+
 }
