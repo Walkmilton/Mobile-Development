d2ab8083ba47694b64f2c1b152e256728cb8a2f2
Declan <dex.doyle@me.com>
2019-04-05 00:02:36 +0100

    Ability to Add and Remove Items from Fridge

--- Coursework/app/src/main/java/com/example/coursework/FridgeActivity.java
+++ Coursework/app/src/main/java/com/example/coursework/FridgeActivity.java
@@ -1,19 +1,26 @@
 package com.example.coursework;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class FridgeActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
 
 
     MyRecyclerViewAdapter adapter;
-
+    List<String> fridgeArray;
+    private String fridge_text = "";
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +29,7 @@ public class FridgeActivity extends AppCompatActivity implements MyRecyclerViewA
 
 
     // data to populate the RecyclerView with
-    ArrayList<String> fridgeArray = new ArrayList<>();
+        fridgeArray = new ArrayList<>();
         fridgeArray.add("Chicken");
         fridgeArray.add("Milk");
         fridgeArray.add("Eggs");
@@ -37,8 +44,67 @@ public class FridgeActivity extends AppCompatActivity implements MyRecyclerViewA
         recyclerView.setAdapter(adapter);
 }
 
+    public void Add_item_to_fridge(View view) {
+
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("What are you putting in the Fridge?");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+
+        // Specify the type of input expected
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("Enter", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                fridge_text = input.getText().toString();
+                count = adapter.getItemCount();
+                fridgeArray.add(fridge_text);
+                adapter.notifyItemInserted(count + 1);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+
+
+    }
+
+
     @Override
-    public void onItemClick(View view, int position) {
-        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    public void onItemClick(View view, final int position) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Do you wish to remove " + adapter.getItem(position) + " from the fridge?");
+
+        //builder.setView(view);
+
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                fridgeArray.remove(position);
+                adapter.notifyItemRemoved(position);
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+
+        //Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 }
